9fb447156dd9f216f0a454389d2758fcf6bed996
giessepi <nalouti.youssef@gmailcom>
2025-01-23 23:26:33 +0100

    Correction des fichiers urls.py et amélioration des routes

--- dusty_sugar/urls.py
+++ dusty_sugar/urls.py
@@ -1,8 +1,11 @@
+# urls.py dans dusty_sugar/dusty_sugar
+
+from django.contrib import admin
 from django.http import HttpResponse
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('', lambda request: HttpResponse("Bienvenue sur Dusty Sugar!"), name='home'),
-    path('admin/', admin.site.urls),
-    # Ajoutez vos autres routes ici
+    path('', lambda request: HttpResponse("Bienvenue sur Dusty Sugar!"), name='home'),  # Page d'accueil
+    path('admin/', admin.site.urls),  # Interface admin
+    path('api/', include('gestion.urls')),  # Inclure les URLs de gestion sous "/api/"
 ]
--- urls.py
+++ urls.py
@@ -1,7 +1,11 @@
+# urls.py dans dusty_sugar/dusty_sugar
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # URL pour l'interface admin
+    path('', lambda request: HttpResponse("Bienvenue sur Dusty Sugar!"), name='home'),  # Page d'accueil
+    path('admin/', admin.site.urls),  # Interface admin
     path('api/', include('gestion.urls')),  # Inclure les URLs de gestion sous "/api/"
 ]
