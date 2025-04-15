0a874ba17fb4f8f2c27a39f8bea964fc54f583ec
giessepi <nalouti.youssef@gmailcom>
2025-04-15 04:16:56 +0200

    💾 Mise à jour version stable : add dj-databes-url

--- gestion/dusty_sugar/settings.py
+++ gestion/dusty_sugar/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,12 +54,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dusty_sugar.wsgi.application'
 
-# Base de données (SQLite par défaut pour le local)
+# Base de données — automatique entre local (SQLite) et Render (PostgreSQL)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Authentification
@@ -85,4 +87,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Champ auto par défaut
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
